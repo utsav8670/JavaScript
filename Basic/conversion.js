ff0c5cfa4209d626ba2de37f5ae2a6fc88788744
utsav8670 <37511234+utsav8670@users.noreply.github.com>
2023-12-13 13:10:31 +0000

    conversion in javascript

---
+++ Basic/conversion.js
@@ -0,0 +1,19 @@
+let score = "33"
+let score1 = "33anc"
+let score2 = null
+console.log(typeof score)
+
+let valueinnumber = Number(score)
+console.log(valueinnumber);
+
+let valueinboolean = Boolean(score)
+console.log(valueinboolean);
+
+let valueinnumber1 = Number(score1)
+console.log(typeof valueinnumber1);
+console.log(valueinnumber1);
+//the type of valueinnumber1 is Nna (NOt a number)
+
+let valueinnumber2 = Number(score2)
+console.log(typeof valueinnumber2);
+console.log(valueinnumber2);
