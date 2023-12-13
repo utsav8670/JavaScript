a5b78d8191c1928503b7d9c9f34a6e39b7216c39
utsav8670 <37511234+utsav8670@users.noreply.github.com>
2023-12-13 11:54:22 +0000

    datatype

---
+++ Basic/datatype.js
@@ -0,0 +1,16 @@
+"use scrict" // treat all the js code as newer version
+
+//number = 2 to power 53
+//Bigint 
+// Boolean => true/false
+//null
+//undefined 
+//string = ""
+//symbol = Unique
+//Object
+
+console.log(typeof "Hello");
+console.table([typeof null,undefined])
+/* type of null is object
+type of undefined is undefined
+*/
